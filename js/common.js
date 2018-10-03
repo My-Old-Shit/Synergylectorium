91c4928931c52875176467bde5b9ba02176f7a1c
Dmitry <dmitry.8060@gmail.com>
2018-10-03 20:27:33 +0300

    SL 1

--- js/common.js
+++ js/common.js
@@ -1,12 +1,3 @@
-var elem = document.querySelector('input');
-var ul = document.querySelector('ul');
-
-elem.onclick = function () {
-
-  ul.classList.add("down");
-
-}
-
 jQuery(document).ready(function() {
 
 	jQuery('input:text, textarea').addClass('styler');
@@ -15,6 +6,59 @@ jQuery(document).ready(function() {
 });
 
 
-
 if(window.matchMedia('(max-width: 1000px)').matches)
 	{ document.getElementById('one').remove()}
+
+
+$(document).ready(function() {
+	$(".various").fancybox({
+		maxWidth	  : 600,
+		maxHeight	  : 500,
+		fitToView	  : false,
+		width		    : '100%',
+		height		  : '100%',
+		autoSize	  : false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none',
+		overlayShow : true,
+		overlayColor: '#545'
+	});
+});
+
+
+$(document).ready(function() {
+	$(".open-form").fancybox({
+		maxWidth	  : 600,
+		maxHeight 	: 600,
+		fitToView 	: false,
+		width	  	  : '100%',
+		height		  : '100%',
+		autoSize	  : false,
+		closeClick	: false,
+		openEffect	: 'fade',
+		closeEffect	: 'fade',
+		padding: 5,
+		margin:  [5, 30, 5, 30],
+		autoResize: true,
+		closeBtn: true,
+		overlayShow : true,
+		overlayColor: '#545',
+		overlayOpacity: 0.9,
+		overlayColor: '#000'
+	});
+});
+
+$(document).ready(function(){
+    $('.fancybox-media').attr('rel', 'media-gallery').fancybox({
+        openEffect : 'none',
+        closeEffect : 'none',
+        prevEffect : 'none',
+        nextEffect : 'none',
+        arrows : false,
+        helpers : {
+            media : {},
+            buttons : {}
+        }
+    });
+});
